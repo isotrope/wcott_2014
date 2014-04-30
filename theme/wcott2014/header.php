b4d06315c890c78dbee578e50fa691fe1213ec4d
Michal Bluma <clients@isotrope.net>
2014-04-30 14:45:36 -0400

    Ten posts loop and styling pretty much there

--- theme/wcott2014/header.php
+++ theme/wcott2014/header.php
@@ -22,12 +22,12 @@
 <div id="page" class="hfeed site">
 
 	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
+		<div class="site-branding inner">
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		</div>
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
+		<nav id="site-navigation" class="main-navigation inner" role="navigation">
 			<button class="menu-toggle"><?php _e( 'Primary Menu', 'wcott2014' ); ?></button>
 			<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'wcott2014' ); ?></a>
 
